b24a91c15d02c4ae39cc2a67c981d75fe9644957
taras <tgamanov@gmail.com>
2017-08-22 23:40:28 +0300

    Module 3. Rendering Flow 3.5.1

---
+++ app/code/Training/Test3/Controller/Block/Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taras
+ * Date: 8/22/17
+ * Time: 11:28 PM
+ */
+
+namespace Training\Test3\Controller\Block;
+
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+public function execute() //call me with training3/block/index
+{
+   $layout = $this->_view->getLayout();
+   $block = $layout->createBlock('Training\Test3\Block\Test');
+   $this->getResponse()->appendBody($block->toHtml());
+}
+}
