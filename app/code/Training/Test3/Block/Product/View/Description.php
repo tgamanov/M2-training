9a5b3251d9d24b317bb9801b49de58da708b17bf
taras <tgamanov@gmail.com>
2017-08-24 20:21:32 +0300

    Module 3. Rendering Flow 3.6.3

--- app/code/Training/Test3/Block/Product/View/Description.php
+++ app/code/Training/Test3/Block/Product/View/Description.php
@@ -8,6 +8,7 @@ class Description extends \Magento\Framework\View\Element\AbstractBlock
 {
     public function beforeToHtml(\Magento\Catalog\Block\Product\View\Description $originalBlock)
     {
-        $originalBlock->getProduct()->setDescription('Test description');
+//        $originalBlock->getProduct()->setDescription('Test description'); // task 2.3
+        $originalBlock->setTemplate('Training_Test3::description.phtml');
     }
 }
