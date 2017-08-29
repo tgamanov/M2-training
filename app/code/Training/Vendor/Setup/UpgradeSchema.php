ce09b2bf5e08046962e08500e10bc7bab1fc0b16
taras <tgamanov@gmail.com>
2017-08-29 23:36:23 +0300

    Module 4. Setup Scripts & Setup Resources 4.4.2

---
+++ app/code/Training/Vendor/Setup/UpgradeSchema.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Training\Vendor\Setup;
+
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if (version_compare($context->getVersion(), '0.0.3') < 0) {
+
+            $installer = $setup;
+            $installer->startSetup();
+
+            $installer->getConnection()->addColumn(
+                $installer->getTable('training_vendor_entity'),
+                'comment',
+                array(
+                    'type' => Table::TYPE_TEXT,
+                    'nullable' => true,
+                    'length' => '1k',
+                    'comment' => 'Comment Field'
+                )
+            );
+
+            $installer->endSetup();
+        }
+    }
+}
