a08a16f5bfbcddd842904c54823a083ef99a5c6d
taras <tgamanov@gmail.com>
2017-09-10 09:55:27 +0300

    magento_2_crud_models_for_database_access/ var_dump("I am Here");

---
+++ app/code/Pulsestorm/ToDoCrud/Block/Main.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pulsestorm\ToDoCrud\Block;
+class Main extends \Magento\Framework\View\Element\Template
+{
+    function _prepareLayout(){
+        var_dump("I am Here");
+        exit;
+    }
+}
