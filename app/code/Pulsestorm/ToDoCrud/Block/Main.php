688edf032e4a1348dff6c57ab43fbcfa4d32f10f
taras <tgamanov@gmail.com>
2017-09-13 09:43:39 +0300

    magento_2_crud_models_for_database_access/ var_dump("I am Here");

--- app/code/Pulsestorm/ToDoCrud/Block/Main.php
+++ app/code/Pulsestorm/ToDoCrud/Block/Main.php
@@ -2,8 +2,32 @@
 namespace Pulsestorm\ToDoCrud\Block;
 class Main extends \Magento\Framework\View\Element\Template
 {
-    function _prepareLayout(){
-        var_dump("I am Here");
+    protected $toDoFactory;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Pulsestorm\ToDoCrud\Model\TodoItemFactory $toDoFactory
+    )
+    {
+        $this->toDoFactory = $toDoFactory;
+        parent::__construct($context);
+    }
+
+    function _prepareLayout()
+    {
+//        $todo = $this->toDoFactory->create();
+//        $todo->setData('title','Finish my Magento article')
+//            ->save();
+//        var_dump('Done');
+//        exit;
+
+        $todo = $this->toDoFactory->create();
+
+        $todo = $todo->load(1);
+        var_dump($todo->getData());
+
+        var_dump($todo->getTitle());
+
+        var_dump($todo->getData('title'));
         exit;
     }
 }
